6411092cd15da5f18f6d85eb993116606cd97db9
BaadMaro <72421091+BaadMaro@users.noreply.github.com>
2022-08-06 23:30:45 +0100

    cleanp ip from headers

--- DoliBrute.py
+++ DoliBrute.py
@@ -58,11 +58,11 @@ def auth (base_url, username, passwords):
             headers["Connection"] = "keep-alive"
             headers["Cache-Control"] = "max-age=0"
             headers["Upgrade-Insecure-Requests"] = "1"
-            headers["Origin"] = "http://192.168.1.110"
+            headers["Origin"] = base_url
             headers["Content-Type"] = "application/x-www-form-urlencoded"
             headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36"
             headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-            headers["Referer"] = "http://192.168.1.110/admin/index.php?mainmenu=home"
+            headers["Referer"] = f"{base_url}admin/index.php?mainmenu=home"
             headers["Accept-Language"] = "en-US,en;q=0.9,ar;q=0.8,fr;q=0.7"
 
             # you can use json object or f string format
